a98ba20e4f2df05f617463478580d7c038de4582
Michaela Jakubikova <jakubi.mich@gmail.com>
2024-02-21 19:24:30 +0100

    uprava trid

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let obrazek = document.querySelector("#zprava3 img")
 bodyElement.style.backgroundColor = "#e9e9e9"
 news.style.backgroundColor = "white"
 news.style.maxWidth = "60rem"
-nadpis.className = "news__title"
+nadpis.classList.add("news__title")
 nadpis.textContent = "Aktuální novinky"
-prvni.className = "post--main"
+prvni.classList.add("post--main")
 obrazek.src = "images/zprava3-novy.jpg"
